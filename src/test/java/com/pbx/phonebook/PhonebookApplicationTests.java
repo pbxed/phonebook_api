84eaec39efb1515e595b2decd3adaef705ffcc67
pbxed <jshenolikar@gmail.com>
2019-12-06 00:46:04 +0000

    tests

--- src/test/java/com/pbx/phonebook/PhonebookApplicationTests.java
+++ src/test/java/com/pbx/phonebook/PhonebookApplicationTests.java
@@ -1,13 +1,41 @@
 package com.pbx.phonebook;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
-class PhonebookApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = PhonebookApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PhonebookApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    private String getRootUrl() {
+        return "http://localhost:" + port;
     }
 
+
+    @Test
+    public void testGetAllUsers() {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/api/users",
+                HttpMethod.GET, entity, String.class);
+
+        Assert.assertNotNull(response.getBody());
+    }
 }
